192145e0e6cf40f3e830d97b2b9f4fa41dacf873
Katherine Mattison <kbmedisto@gmail.com>
2014-05-20 10:47:07 -0400

    first commit ng day two

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,63 @@
+'use strict';
+
+angular.module('ngDayTwoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
+
+  .controller('ArtController', function($scope) {
+	$scope.product = [
+
+		{
+		name: 'Ball of Fire',
+		price: 2000,
+		description: 'This is a Ball of Fire',
+		},
+
+		{
+		name: 'Ball of Fire',
+		price: 2000,
+		description: 'This is a Ball of Fire',
+		},
+
+		{
+		name: 'Ball of Fire',
+		price: 2000,
+		description: 'This is a Ball of Fire',
+		},
+
+		{
+		name: 'Ball of Fire',
+		price: 2000,
+		description: 'This is a Ball of Fire',
+		},
+
+
+
+	]
+
+
+
+
+
+
+  		// $scope.whateverYouWant = [
+  		// 	{
+  		// 		name: "Kat Mattison",
+  		// 		hobby: "reading"
+  		// 	},
+  		// 	{
+  		// 		name: "Calvin Webster",
+  		// 		hobby: "surfing"
+  		// 	},
+  		// 	{},
+  		// 	{},
+  		// 	{},
+
+  		// ];
+  });
+
