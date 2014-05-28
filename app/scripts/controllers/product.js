2c8685b4e90b00bf9f0d4fd6409d3c0e04234a2c
Katherine Mattison <kbmedisto@gmail.com>
2014-05-28 11:37:03 -0400

    edited product controller js, changed styles, edited and made comments in html pages in order to plan how I will edit customer orders LEADING to adding the ADMIN page so that the owner of theart gallery can add in new products, new artists, etc

---
+++ app/scripts/controllers/product.js
@@ -0,0 +1,11 @@
+angular.module('ngDayTwoApp')
+	.controller('ProductCtrl', ['$scope', '$routeParams', '$location', 'ProductService', function($scope, $routeParams, $location, ProductService)  {
+
+		$scope.product = ProductService.showProduct({ id: $routeParams.id});
+
+		$scope.updateProduct = function() {
+			ProductService.updateProduct($scope.product);
+			$location.path("/")
+		}
+
+	}])
